9fb7e014eb731315acd9db6f7987b5a862edc9b0
Nuby <nuby.4register@gmail.com>
2021-05-27 12:49:40 +0300

    Daily commit.

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task14.task1421;
+
+/* 
+Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+/*        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.foo);
+        System.out.println(anotherSingleton.foo);*/
+    }
+}
