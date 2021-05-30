772bdf343b533666c77cc5b8e4cb9735d49ff906
Nuby <nuby.4register@gmail.com>
2021-05-30 22:18:07 +0300

    Daily commit.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1530/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1530;
+
+/* 
+Template pattern
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+/*        TeaMaker tm = new TeaMaker();
+        tm.makeDrink();
+        LatteMaker lm = new LatteMaker();
+        lm.makeDrink();*/
+    }
+}
