64f34468928d9810a7cfc0dc4540dabcb03037b7
Nuby <nuby.4register@gmail.com>
2022-04-15 14:41:08 +0300

    +

--- 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -25,8 +25,67 @@ public class Solution {
         System.out.println("count = " + count2 + ". Должно быть 4");
     }
 
-    public static int getRectangleCount(byte[][] a) {
+    public static int getRectangleCount(byte[][] arr) {
+        byte[][] array = new byte[arr.length][arr[0].length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = arr[i][j];
+            }
+        }
 
-        return 0;
+        int horizontal = array[0].length;
+        int vertical = array.length;
+        int counter;
+        int verticalEnd;
+        int horizontalEnd;
+        int result = 0;
+        for (int i = 0; i < vertical; i++) {
+            for (int j = 0; j < horizontal; j++) {
+                if (array[i][j] == 1) {
+                    verticalEnd = i;
+                    horizontalEnd = j;
+                    counter = i + 1;
+                    System.out.println("a[" + counter + "][" + j + "]");
+                    while (true) {
+                        if (counter >= vertical || (array[counter][j] == 0 && counter < vertical)) {
+                            verticalEnd = counter - 1;
+                            System.out.println("verEnd = " + verticalEnd);
+                            break;
+                        }
+                        counter++;
+                        System.out.println("a[" + counter + "][" + j + "]");
+                    }
+                    //по горизонтали
+                    counter = j + 1;
+                    while (true) {
+                        if (counter >= horizontal || (array[i][counter] == 0 && counter < horizontal)) {
+                            horizontalEnd = counter - 1;
+                            System.out.println("horEnd = " + horizontalEnd);
+                            break;
+                        }
+                        counter++;
+                    }
+                    boolean flag = true;
+                    for (int p = i; p <= verticalEnd; p++)
+                        for (int q = j; q <= horizontalEnd; q++)
+                            if (array[p][q] != 1) {
+                                flag = false;
+                                break;
+                            }
+                    if (flag) {
+                        System.out.println("startI = " + i);
+                        System.out.println("startJ = " + j);
+                        System.out.println("endI = " + verticalEnd);
+                        System.out.println("endJ = " + horizontalEnd);
+                        for (int p = i; p <= verticalEnd; p++)
+                            for (int q = j; q <= horizontalEnd; q++)
+                                array[p][q] = 0;
+                        result++;
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        return result;
     }
 }
